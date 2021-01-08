114a62e478821bc962a9bad266895330b3c73e6e
LeoKnox <LeoKnox@users.noreply.github.com>
2021-01-07 20:30:20 -0800

    Add files via upload

--- start.py
+++ start.py
@@ -6,5 +6,21 @@ cursor = connection.cursor()
 cursor.execute(''' CREATE TABLE IF NOT EXISTS Intro (Name TEXT, char_class TEXT,
     char_race TEXT, level INT)''')
 
+#cursor.execute("INSERT INTO Intro VALUES ('Xingu', 'Fighter', 'Human', 3)")
+
+ourChars = [('Colette', 'Rogue', 'Elf', 2),
+    ('Aelien', 'Mage', 'Daemon', 2)]
+
+#cursor.executemany('Insert INTO Intro VALUES (?,?,?,?)', ourChars)
+our_list = cursor.execute("SELECT * FROM Intro")
+
+for our in our_list:
+    if our[3] == 3:
+        print(our)
+
+cursor.execute("SELECT * FROM Intro")
+
+print(cursor.fetchall())
+
 connection.commit()
 connection.close()
