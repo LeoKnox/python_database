ad5c449b80139812953ca12ba9cf24add173d736
LeoKnox <LeoKnox@users.noreply.github.com>
2021-01-07 20:37:53 -0800

    Add files via upload

--- start.py
+++ start.py
@@ -3,22 +3,29 @@ connection = sqlite3.connect('intro.db')
 
 cursor = connection.cursor()
 
-cursor.execute(''' CREATE TABLE IF NOT EXISTS Intro (Name TEXT, char_class TEXT,
-    char_race TEXT, level INT)''')
+
+#cursor.execute(''' CREATE TABLE IF NOT EXISTS Intro (Name TEXT, char_class TEXT,
+#    char_race TEXT, level INT)''')
 
 #cursor.execute("INSERT INTO Intro VALUES ('Xingu', 'Fighter', 'Human', 3)")
 
-ourChars = [('Colette', 'Rogue', 'Elf', 2),
-    ('Aelien', 'Mage', 'Daemon', 2)]
+#ourChars = [('Colette', 'Rogue', 'Elf', 2),
+#    ('Aelien', 'Mage', 'Daemon', 2)]
 
 #cursor.executemany('Insert INTO Intro VALUES (?,?,?,?)', ourChars)
-our_list = cursor.execute("SELECT * FROM Intro")
+#our_list = cursor.execute("SELECT * FROM Intro")
 
-for our in our_list:
-    if our[3] == 3:
-        print(our)
+#for our in our_list:
+#    if our[3] == 3:
+#        print(our)
 
-cursor.execute("SELECT * FROM Intro")
+#cursor.execute("SELECT * FROM Intro")
+
+#print(cursor.fetchall())
+
+# use tuple variable and ? variable to add variables to query else sql injection attack
+current_level = (3,)
+cursor.execute("SELECT * FROM Intro WHERE level=?", current_level)
 
 print(cursor.fetchall())
 
