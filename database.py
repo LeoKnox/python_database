53cf15d80c07a74847388aecb46274a3df9541b4
LeoKnox <LeoKnox@users.noreply.github.com>
2021-01-08 20:20:13 -0800

    Add files via upload

--- database.py
+++ database.py
@@ -16,3 +16,23 @@ result_set = result_proxy.fetchall()
 
 print(result_set[0])
 print(result_set[:2])
+
+query = db.select([intro]).where(intro.columns.char_race == 'Elf')
+
+result_proxy = connection.execute(query)
+
+result_set = result_proxy.fetchall()
+
+print(result_set[0])
+
+query = intro.insert().values(Name="Ynzon",char_class="Barbarian",char_race="Orc",level=4)
+
+connection.execute(query)
+
+query = db.select([intro])
+
+result_proxy = connection.execute(query)
+
+result_set = result_proxy.fetchall()
+
+print(result_set)
