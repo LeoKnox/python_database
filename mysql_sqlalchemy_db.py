4ce7ad10893818d9823dd00b4022cb9cd3171e90
LeoKnox <LeoKnox@users.noreply.github.com>
2021-01-16 20:59:07 -0800

    Update mysql_sqlalchemy_db.py

--- mysql_sqlalchemy_db.py
+++ mysql_sqlalchemy_db.py
@@ -32,3 +32,24 @@ class Room(Base):
         return "<Room(description='{0}'>".formation(self.description)
 
 Base.metadata.create_all(engine)
+
+session_maker = sessionmaker()
+
+session_maker.configure(bind=engine)
+session = session_maker()
+
+quadro_dungeon_project = Project(name='Quadro Dungeon', description='Off roading dungeon')
+session.add(quadro_dungeon_project)
+session.commit()
+
+rooms = [Room(project_id=quadro_dungeon_project.project_id, description="First generic room"), 
+    Room(project_id=quadro_dungeon_project.project_id, description="Second generic room")]
+
+session.bulk_save_objects(rooms)
+session.commit()
+
+our_project = session.query(Project).filter_by(title='Quadro Dungeon').first()
+print(out_project)
+
+our_rooms = session.query(Room).all()
+print(rooms)
