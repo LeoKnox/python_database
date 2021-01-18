3808b2f70a76757bbcd0657d395dd1b7de9f4d82
LeoKnox <LeoKnox@users.noreply.github.com>
2021-01-17 21:13:02 -0800

    Add files via upload

--- python_csv.py
+++ python_csv.py
@@ -15,17 +15,24 @@ create_query = ''' CREATE TABLE salesperson(
     email_address VARCHAR(255) NOT NULL,
     city VARCHAR(255) NOT NULL,
     state VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id))'''
+    PRIMARY KEY (id),
+    allow_local_infile=True)'''
 
 curs.or.execute("DROP TABLE IF EXISTS salesperson")
 
 cursor.execute(create_query)
 
+'''
 with open('./salesperson.csv', 'r') as f:
     csv_data = csv.reader(f)
     for row in csv_data:
         row_tuple = tuple(row)
         cursor.execute('INSERT INTO salesperson(first_name, last_name, email_address, city, state) VALUES(""%s","%s","%s","%s",%s"), row_tuple')
+        '''
+
+q = '''LOAD DATA LOCAL INFILE '.salesperson.csv INTO TABLE salesperson FIELDS TERMINATED BY ',' ENCLOSED BY '"' (first_name, last_name, email_address, city, state);'''
+
+cursor.execute(q)
 
 connection.commit()
 cursor.execute("SELECT * FROM salesperson LIMIT 10")
